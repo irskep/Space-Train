78aca852816bce1f4bcdbf15da7205b542c35624
Stephen Johnson <srj15@case.edu>
2010-08-26 16:45:21 -0400

    Gave up on windows symlinks

--- settings.py
+++ settings.py
@@ -2,21 +2,12 @@ fullscreen = False
 
 import os
 
-def readlinkabs(l):
-    """
-    Return an absolute path for the destination 
-    of a symlink
-    """
-    assert (os.path.islink(l))
-    p = os.readlink(l)
-    if os.path.isabs(p):
-        return p
-    return os.path.join(os.path.dirname(l), p)
-
-try:
-    
-    from Carbon import File
-    fs, _, _ = File.ResolveAliasFile('resources',1)
-    resources_path = fs.as_pathname()
-except ImportError:
-    resources_path = readlinkabs('resources')
+if os.path.isdir('resources')):
+    resources_path = 'resources'
+else:
+    try:
+        from Carbon import File
+        fs, _, _ = File.ResolveAliasFile('resources',1)
+        resources_path = fs.as_pathname()
+    except ImportError:
+        resources_path = readlinkabs('resources')
