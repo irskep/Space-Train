af79b38d35f726d0094078b50705e25b44e31756
Stephen Johnson <srj15@case.edu>
2010-08-26 16:46:30 -0400

    Dear Steve, test your code before you push it. Sincerely, Steve.

--- settings.py
+++ settings.py
@@ -10,4 +10,4 @@ else:
         fs, _, _ = File.ResolveAliasFile('resources',1)
         resources_path = fs.as_pathname()
     except ImportError:
-        resources_path = readlinkabs('resources')
+        resources_path = 'resources'
