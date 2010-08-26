8b54e4e06ee82803f521cadb483b86f1eb0c0f8e
Stephen Johnson <srj15@case.edu>
2010-08-26 18:00:38 -0400

    Added my own dropbox path to settings.py

--- settings.py
+++ settings.py
@@ -5,10 +5,12 @@ import os
 resource_locations = [
     r"resources",   # the 'r' turns off escape sequences
     r"C:\Users\Fred\Documents\My Dropbox\resources",
+    r"/Users/stephen/Dropbox/resources",
     r"YOUR_PATH_HERE"
 ]
 
 for location in resource_locations:
+    print 'trying', location
     if os.path.isdir(location):
         resources_path = location
         break
@@ -17,6 +19,7 @@ for location in resource_locations:
             from Carbon import File
             fs, _, _ = File.ResolveAliasFile('resources',1)
             resources_path = fs.as_pathname()
+            print resources_path
             break
         except:
             pass
