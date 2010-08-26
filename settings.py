cb264e22a70732129c30b1105a9c57e84c55ade4
Stephen Johnson <srj15@case.edu>
2010-08-26 17:03:56 -0400

    Started providing settings.resource_locations to get around the whole symlink issue altogether

--- settings.py
+++ settings.py
@@ -2,12 +2,21 @@ fullscreen = False
 
 import os
 
-if os.path.isdir('resources'):
-    resources_path = 'resources'
-else:
-    try:
-        from Carbon import File
-        fs, _, _ = File.ResolveAliasFile('resources',1)
-        resources_path = fs.as_pathname()
-    except ImportError:
+resource_locations = [
+    r"resources",   # the 'r' turns off escape sequences
+    r"C:\PATH\TO\FRED'S\SPECIAL\FOLDER",
+    r"YOUR_PATH_HERE"
+]
+
+for location in resource_locations:
+    if os.path.isdir(location):
         resources_path = 'resources'
+        break
+    else:
+        try:
+            from Carbon import File
+            fs, _, _ = File.ResolveAliasFile('resources',1)
+            resources_path = fs.as_pathname()
+            break
+        except:
+            pass
