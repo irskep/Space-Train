db92d68c35c490d50c452cb25b2005f59fe34a55
Stephen Johnson <srj15@case.edu>
2010-08-26 15:25:13 -0400

    Added spot for Windows shortcut-following to go

--- settings.py
+++ settings.py
@@ -5,4 +5,8 @@ try:
     fs, _, _ = File.ResolveAliasFile('resources',1)
     resources_path = fs.as_pathname()
 except ImportError:
-    resources_path = 'resources'
+    try:
+        # Delete this line and replace it with Windows shortcut-following code
+        raise ImportError
+    except ImportError:
+        resources_path = 'resources'
