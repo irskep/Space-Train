4c9bba3be4fa01c4b00aa6c249a6865e7f25cd89
Stephen Johnson <srj15@case.edu>
2010-08-26 16:06:38 -0400

    Possible fix for Windows shortcut issue

--- settings.py
+++ settings.py
@@ -1,12 +1,22 @@
 fullscreen = False
 
+import os
+
+def readlinkabs(l):
+    """
+    Return an absolute path for the destination 
+    of a symlink
+    """
+    assert (os.path.islink(l))
+    p = os.readlink(l)
+    if os.path.isabs(p):
+        return p
+    return os.path.join(os.path.dirname(l), p)
+
 try:
+    
     from Carbon import File
     fs, _, _ = File.ResolveAliasFile('resources',1)
     resources_path = fs.as_pathname()
 except ImportError:
-    try:
-        # Delete this line and replace it with Windows shortcut-following code
-        raise ImportError
-    except ImportError:
-        resources_path = 'resources'
+    resources_path = readlinkabs('resources')
