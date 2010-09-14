3a2f8e0eb2f2a131bb37c670fe1ffff303755b8c
Fred Hatfull <fxh32@case.edu>
2010-09-14 15:02:49 -0400

    Preliminary inventory drawing

--- engine/gamehandler.py
+++ engine/gamehandler.py
@@ -1,12 +1,18 @@
 import settings
 
-import scene, scenehandler
+import scene, scenehandler, ui
 
 class GameHandler(object):
     """This class will be useful when scene transitions are implemented."""
     def __init__(self, first_scene):
         self.scene = scene.Scene(first_scene)
         self.scene_handler = scenehandler.SceneHandler(self.scene)
+        self.ui = ui.UI()
         self.update = self.scene_handler.scene.update
-        self.draw = self.scene_handler.scene.draw
     
+    # method to draw appropriate elements
+    # unsure if this change is proper (i.e. calling draw() rather than assigning a function to draw()
+    # in the constructor. these are equivalent?
+    def draw(self):
+        self.scene_handler.scene.draw()
+        self.ui.draw()
