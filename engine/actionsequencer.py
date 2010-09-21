067a55d0d75daba1f593086b93877a7f8f45d686
Stephen Johnson <srj15@case.edu>
2010-09-21 13:24:30 -0400

    Factored action sequence stuff out into ActionSequencer

---
+++ engine/actionsequencer.py
@@ -0,0 +1,58 @@
+import os, json, collections
+
+import pyglet
+
+import const, interpolator, util
+
+class ActionSequencer(object):
+    """
+    Action sequences consist of a list of lists of tuples. Like this:
+    [   # Sequence of actions
+        [   # Actions to perform simultaneously
+            (function_to_call, (positional_arguments_to_function))
+        ]
+    ]
+    
+    This function, next_action(), pops an action off the front of the list and starts it.
+    The actions themselves are responsible for calling next_action() when they are finished.
+    
+    Example:
+    [
+        [
+            (move_to, ((10, 10), 'walk_left')),
+            (scale_between, (0.5, 2.0, 2.0)),   #imaginary: scale_between(start, end, duration)
+        ], 
+        [
+            (next_action, ())
+        ]
+    ]
+    In this example, the ActionSequencer will (move from its current position to the point (10, 10))
+    while (scaling between half and double size over a duration of two seconds). It will
+    be up to move_to() and/or scale_between() to cause next_action() to be called at some
+    point, either by setting done_function on an Interpolator or by setting a pyglet timer.
+    """
+    
+    def __init__(self):
+        self.actions = collections.deque()
+        self.blocking_actions = 0
+    
+    def block(self):
+        self.blocking_actions += 1
+    
+    def unblock(self):
+        if self.blocking_actions > 0:
+            self.blocking_actions -= 1
+    
+    def next_action(self, ending_action=None):
+        """Start the next action in the queue"""
+        self.unblock()
+        if len(self.actions) > 0:
+            action_list = self.actions.popleft()
+            self.block()
+            for action in action_list:
+                # Call first item (a function) in the action tuple with the parameters listed
+                # as the second item in the tuple
+                # Example: (move_to, ((x, y), 'walk_right'))
+                action[0](*action[1])
+    
+
