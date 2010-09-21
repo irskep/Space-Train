cce1ab321bca04e8fd19eee24f4a7f792b121e38
Stephen Johnson <srj15@case.edu>
2010-09-21 13:25:30 -0400

    SceneHandler now inherits from ActionSequencer

--- engine/scenehandler.py
+++ engine/scenehandler.py
@@ -2,10 +2,11 @@ import json
 
 import pyglet
 
-import gamestate
+import gamestate, actionsequencer
 
-class SceneHandler(object):
+class SceneHandler(actionsequencer.ActionSequencer):
     def __init__(self, scene_object):
+        super(self, SceneHandler).__init__()
         self.scene = scene_object
         gamestate.main_window.push_handlers(self.scene)
     
