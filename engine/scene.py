ebd71c96f628e3a8f62c427d335f374b0e0d028c
lurkingfridge79 <lurkingfridge79@gmail.com>
2010-09-22 17:33:08 -0400

    Maturation of CAM. Still not stable (causes the actor selected to block() for some reason

--- engine/scene.py
+++ engine/scene.py
@@ -74,15 +74,19 @@ class Scene(interpolator.InterpolatorController):
     # Events
     
     def on_mouse_release(self, x, y, button, modifiers):
+        print "Mouse event received"
         clicked_actor = self.actor_under_point(x, y)
         if clicked_actor:
+            self.ui.actor_clicked(clicked_actor)
             if hasattr(self.module, 'actor_clicked'):
                 self.module.actor_clicked(clicked_actor)
-            self.ui.actor_clicked(clicked_actor)
         elif self.actors.has_key("main"):
+            print "No clicked_actor. We have a main actor"
             # Send main actor to click location according to actor's moving behavior
             main = self.actors["main"]
+            print "Main actor blocking actions: %d" % main.blocking_actions
             if main.prepare_move(*self.camera.mouse_to_canvas(x, y)):
+                print "O hai!"
                 main.next_action()
     
     
