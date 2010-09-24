d6d9094f96aa40cdcb45bc69455d7a98af80758f
Tyler Goeringer <tyler.goeringer@gmail.com>
2010-09-23 23:32:20 -0400

    Forgot to add the second scene in last commit.

---
+++ game/scenes/test_2/__init__.py
@@ -0,0 +1,15 @@
+from engine import actor
+
+# myscene is set by scene.py
+myscene = None
+
+def init():
+    print "Initializing script for", myscene
+    print "Not changing anything, accepting defaults"
+
+def handle_event(event, *args):
+    print "Handled", event, "with", args
+
+def actor_clicked(clicked_actor):
+    clicked_actor.prepare_jump()
+    clicked_actor.next_action()
