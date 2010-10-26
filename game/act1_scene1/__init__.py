7a183e745d2398847372d3afc15c61b6fdbbb7ca
Fred Hatfull <fxh32@case.edu>
2010-10-25 22:52:42 -0400

    Cart lady's event on walk complete not firing correctly.

--- game/act1_scene1/__init__.py
+++ game/act1_scene1/__init__.py
@@ -9,11 +9,19 @@ myscene = None
 
 def init():
     myscene.ui.inventory.visible = True
+    myscene.actors['levity'].prepare_walkpath_move("levity_inga")
+    myscene.actors['levity'].next_action()
     #text.init(myscene)
 
 def inga_walk(actor, point):
     pass
-
+    
+def levity_walk(actor, point):
+    if point == "levity_inga":
+        #begin convo
+        print "updating state"
+        actor.update_state("stand_right")
+    
 def ask_about_beans():
     myscene.convo.begin_conversation('beans_1')
     bean_salesman = myscene.actors['bean_salesman']
@@ -22,7 +30,8 @@ def end_conversation(convo_name):
     myscene.actors['main'].update_state('stand_front')
 
 walk_handlers = {
-    'main': inga_walk
+    'main': inga_walk,
+    'levity': levity_walk
 }
 
 def handle_event(event, *args):
