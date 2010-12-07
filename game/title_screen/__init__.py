30023bb2aad621b01ecdfd603b411e4e9fa45a2f
Stephen Johnson <srj15@case.edu>
2010-12-06 19:24:51 -0500

    All Aboard always expands into screen (had forgotten to delete pulse interpolator)

--- game/title_screen/__init__.py
+++ game/title_screen/__init__.py
@@ -4,12 +4,15 @@ from engine.interpolator import PulseInterpolator, LinearInterpolator
 # myscene is set by scene.py
 myscene = None
 
+go_interp = None
+
 def init(fresh=True):
+    global go_interp
     myscene.ui.inventory.visible = False
     title = myscene.actors['go']
     title.use_mask_to_detect_clicks = False
-    interp = PulseInterpolator(title.sprite, 'scale', 0.9, 1.0, speed=4)
-    myscene.add_interpolator(interp)
+    go_interp = PulseInterpolator(title.sprite, 'scale', 0.9, 1.0, speed=4)
+    myscene.add_interpolator(go_interp)
 
 def handle_event(event, *args):
     pass
@@ -19,6 +22,7 @@ def next(*args, **kwargs):
 
 def actor_clicked(clicked_actor):
     if clicked_actor.identifier == "go":
+        go_interp.stop = True
         myscene.play_sound("new_game")
         interp = LinearInterpolator(myscene.actors['go'].sprite, 'scale', end=3.0, duration=0.72)
         myscene.add_interpolator(interp)
